dd0b0e041af1ba114c68df973fd992e9366bad4a
dell <help.essensol@gmail.com>
2019-10-25 14:13:53 +0530

    Some Changes

--- app/src/main/java/com/essensol/techmeq/Adapters/ProductsAdapter.java
+++ app/src/main/java/com/essensol/techmeq/Adapters/ProductsAdapter.java
@@ -56,7 +56,7 @@ public  class ProductsAdapter extends  RecyclerView.Adapter<ProductsAdapter.view
 
         int devicewidth = displaymetrics.widthPixels/2;
 
-        int deviceheight = displaymetrics.heightPixels /3;
+        int deviceheight = displaymetrics.heightPixels /6;
 
         holder.imageid.getLayoutParams().height = deviceheight;
 
